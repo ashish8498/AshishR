adadad5cf43b5d0766404d36b7962e750ff030db
AD Technologies <AD Technologies>
2020-07-10 16:33:55 +0530

    Adding Files

---
+++ Ashish Banuba Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Ashish Banuba Demo
+//
+//  Created by A D Technology on 10/07/20.
+//  Copyright © 2020 A D Technology. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ashish Banuba DemoTests/Ashish_Banuba_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ashish_Banuba_DemoTests.swift
+//  Ashish Banuba DemoTests
+//
+//  Created by A D Technology on 10/07/20.
+//  Copyright © 2020 A D Technology. All rights reserved.
+//
+
+import XCTest
+@testable import Ashish_Banuba_Demo
+
+class Ashish_Banuba_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
